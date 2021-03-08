91c873cb668a7198a49d29e893492db5b1629d22
Demilaris <niamrouze@gmail.com>
2021-03-08 20:09:55 +0300

    car behaviour right and left analysis
    (cherry picked from commit 8cd33b69a25a99c8a22f95c777822d1a5d94d37a)

--- CarBehaviour.py
+++ CarBehaviour.py
@@ -7,36 +7,56 @@ car_not_stop = cv2.imread('./testing_data/car_not_stopped.png', cv2.IMREAD_COLOR
 
 def CarBehaviour(img1,img2):
 
-    bbox_rlight = tl.TailDetector(img1)
-    bbox_rlight_2 = tl.TailDetector(img2)
+    bbox_img1 = tl.TailDetector(img1)
+    bbox_img2 = tl.TailDetector(img2)
 
-    x, y, w, h =  bbox_rlight[0][0][0], bbox_rlight[0][0][1], bbox_rlight[0][1][0],bbox_rlight[0][1][1]
-    right_light_img_1 = img1[y:y + h, x:x + w]
-    cv2.imshow('right-img1',right_light_img_1)
+    x, y, w, h =  bbox_img1[0][0][0], bbox_img1[0][0][1], bbox_img1[0][1][0],bbox_img1[0][1][1]
+    right_light_img1 = img1[y:y + h, x:x + w]
+    x, y, w, h = bbox_img1[1][0][0], bbox_img1[1][0][1], bbox_img1[1][1][0], bbox_img1[1][1][1]
+    left_light_img1 = img1[y:y + h, x:x + w]
 
 
-    x, y, w, h = bbox_rlight_2[0][0][0], bbox_rlight_2[0][0][1], bbox_rlight_2[0][1][0], bbox_rlight_2[0][1][1]
-    left_light_img_1 = img2[y:y + h, x:x + w]
-    cv2.imshow('right-img2', left_light_img_1)
+    x, y, w, h = bbox_img2[0][0][0], bbox_img2[0][0][1], bbox_img2[0][1][0], bbox_img2[0][1][1]
+    right_light_img2 = img2[y:y + h, x:x + w]
+    x, y, w, h = bbox_img2[0][0][0], bbox_img2[0][0][1], bbox_img2[0][1][0], bbox_img2[0][1][1]
+    left_light_img2 = img2[y:y + h, x:x + w]
+
     #самый тупой способ
     status = None
-    img1_yCrCb = cv2.cvtColor(right_light_img_1, cv2.COLOR_BGR2YCrCb)
-    img2_yCrCb = cv2.cvtColor(left_light_img_1, cv2.COLOR_BGR2YCrCb)
+    img1_yCrCb_r = cv2.cvtColor(right_light_img1, cv2.COLOR_BGR2YCrCb)
+    img1_yCrCb_l = cv2.cvtColor(left_light_img1, cv2.COLOR_BGR2YCrCb)
 
-    img1_Y,img1_Cr,img1_Cb = cv2.split(img1_yCrCb)
-    img2_Y, img2_Cr, img2_Cb = cv2.split(img2_yCrCb)
+    img2_yCrCb_r = cv2.cvtColor(right_light_img2, cv2.COLOR_BGR2YCrCb)
+    img2_yCrCb_l = cv2.cvtColor(left_light_img2, cv2.COLOR_BGR2YCrCb)
 
-    ret, thresh1 = cv2.threshold(img1_Cr, 200, 255, cv2.THRESH_BINARY)
-    ret2, thresh2 = cv2.threshold(img2_Cr, 200, 255, cv2.THRESH_BINARY)
-    cv2.imshow('thres1-img1', thresh1)
-    cv2.imshow('thres2-img2', thresh2)
-    non1 = cv2.countNonZero(thresh1)
-    non2 = cv2.countNonZero(thresh2)
+    img1_Y_r,img1_Cr_r,img1_Cb_r = cv2.split(img1_yCrCb_r)
+    img1_Y_l, img1_Cr_l, img1_Cb_l = cv2.split(img1_yCrCb_l)
+
+    img2_Y_r,img2_Cr_r,img2_Cb_r = cv2.split(img2_yCrCb_r)
+    img2_Y_l, img2_Cr_l, img2_Cb_l = cv2.split(img2_yCrCb_l)
+
+
+    ret, thresh1_r = cv2.threshold(img1_Cr_r, 200, 255, cv2.THRESH_BINARY)
+    ret, thresh1_l = cv2.threshold(img1_Cr_l, 200, 255, cv2.THRESH_BINARY)
+
+    ret2, thresh2_r = cv2.threshold(img2_Cr_r, 200, 255, cv2.THRESH_BINARY)
+    ret, thresh2_l = cv2.threshold(img2_Cr_l, 200, 255, cv2.THRESH_BINARY)
+
+
+    non1_r = cv2.countNonZero(thresh1_r)
+    non1_l = cv2.countNonZero(thresh1_l)
+
+    non2_r = cv2.countNonZero(thresh2_r)
+    non2_l = cv2.countNonZero(thresh2_l)
+
+    non1 = non1_r+non1_l
+    non2 = non2_r + non2_l
+
     difference = abs(non1 - non2)
 
     print(difference)
     #не знаю как корректнее сделать пока что от балды
-    if difference > 200:
+    if difference > 1000:
         status = 'braking'
     else:
         status ='driving'
